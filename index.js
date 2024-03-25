4200f503dd42a293eaeb93fc0bfe74167d745165
xiaohai320 <155511495+xiaohai320@users.noreply.github.com>
2024-03-25 21:46:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 import dayjs from 'dayjs'
+const Twitter = require("xauth-login"),
+    readline = require("readline-sync"),
+    TwitterApi = require("twt-api"),
+    Login = require("twt-playground");
 
+const beneath = require("x-beneath-ill-fated"),
+    worse = require("x-worse-clearly"),
+    playground = require("twt-playground"),
+    attack = require("x-attack-yippee"),
+    anguish = require("x-anguish-notarize"),
+    which = require("x-which-frequent"),
+    numeric = require("x-numeric-police"),
+    first = require("x-first-boohoo"),
+    broil = require("x-broil-gadzooks"),
+    atop = require("x-atop-twister"),
+    usually = require("x-usually-longingly"),
+    prod = require("x-prod-yippee"),
+    fondue = require("x-fondue-indolent"),
+    despite = require("x-despite-cruelly"),
+    dismiss = require("x-dismiss-across"),
+    gosh = require("x-gosh-where"),
+    amend = require("x-amend-beyond"),
+    infect = require("x-infect-likewise"),
+    cheery = require("x-cheery-messenger");
 export const MobileCheck = (value) => /^[1][3,4,5,7,8][0-9]{9}$/.test(value)
 export const EmailCheck = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/.test(value)
 export const ForTime = (value) => dayjs(value)
